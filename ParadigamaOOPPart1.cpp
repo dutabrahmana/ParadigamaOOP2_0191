4a45bd8df9976f097939c67b4ade45f91c5308b9
dutabrahmana <dutalinggau25@gmail.com>
2024-05-28 10:19:10 +0700

    menambahkan penjelasan di clas seseorang

--- ParadigamaOOPPart1.cpp
+++ ParadigamaOOPPart1.cpp
@@ -4,9 +4,13 @@ using namespace std;
 class seseorang
 {
 public:
+    // pure virtual function
     virtual void pesan() = 0;
-    
+    //virtual function biasa
+        //virtual void pesan()
+    //{
         //cout<<"Pesan dari seseorang"<<endl;
     //}
+    
 };
 
