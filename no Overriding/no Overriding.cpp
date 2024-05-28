50dba649dd6a12c99f1bfd672b8c45dd2efdba80
dutabrahmana <dutalinggau25@gmail.com>
2024-05-28 10:49:52 +0700

    menyelesaikan project noPewarisan

---
+++ no Overriding/no Overriding.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahkan final sesuddah nama class
+//untuk mencegah adanya overriding
+
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Fnction dari derived Class";
+    }
+
+};
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
+
+
+
+
