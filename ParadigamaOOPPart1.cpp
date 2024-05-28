8fcdc59d35b8a115ea6fb8c827c84fa91b068327
dutabrahmana <dutalinggau25@gmail.com>
2024-05-28 10:29:56 +0700

    menambahkan int main

--- ParadigamaOOPPart1.cpp
+++ ParadigamaOOPPart1.cpp
@@ -26,4 +26,20 @@ public:
     }
 };
 
+int main() {
+    seseorang *obyek;
+    joko  a;
+    lia  b;
+
+    obyek =   &a;
+    obyek-> pesan();
+    obyek =  &b;
+    obyek-> pesan();
+    //akses method pesan () dari clas seseorng
+    //a.seseorang::pesan();
+    
+
+    return 0;
+}
+
 
